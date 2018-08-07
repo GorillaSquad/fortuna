a92ff0bda9c300ed8f1151ddc0ccf51d93829c78
Jason <dezinated@gmail.com>
2018-08-07 14:21:02 -0400

    chatroom

--- app/src/main/java/com/example/jason/myapplication/ChatRoom.java
+++ app/src/main/java/com/example/jason/myapplication/ChatRoom.java
@@ -1,4 +1,19 @@
 package com.example.jason.myapplication;
 
-public class ChatRoom {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+
+import com.inmobi.ads.InMobiBanner;
+import com.inmobi.sdk.InMobiSdk;
+
+public class ChatRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chatroom);
+
+    }
 }
