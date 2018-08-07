2b3b31387e05dc4db2c6871eaa75b2d61d02fc24
Jason <dezinated@gmail.com>
2018-08-07 14:40:45 -0400

    chatroom work

--- app/src/main/java/com/example/jason/myapplication/ChatRoom.java
+++ app/src/main/java/com/example/jason/myapplication/ChatRoom.java
@@ -2,6 +2,8 @@ package com.example.jason.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.RelativeLayout;
 
 import com.inmobi.ads.InMobiBanner;
@@ -9,11 +11,28 @@ import com.inmobi.sdk.InMobiSdk;
 
 public class ChatRoom extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chatroom);
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.chatRecyclerView);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new ChatAdapter(new String[]{"test4", "test2", "test3"});
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
